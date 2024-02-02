274d2236f7acd4dfdaa682fdbfdaec897faa24e3
Jairaghav123 <jairaghav99@gmail.com>
2024-02-02 17:47:15 +0530

    change v3

--- lib/regenmode.dart
+++ lib/regenmode.dart
@@ -475,7 +475,7 @@ class _regenmode extends State<regenmode> {
                                               // InfoItem(label: 'Battery', value: '80%'), // Replace with actual values
                                               Padding(
                                                 padding: const EdgeInsets.only(left:20.0),
-                                                child: InfoItem(label: 'Pressure', value: (psi1 != null )? "               $psi1 psi":"00000 psi " ?? "0", icon: Icons.speed),
+                                                child: InfoItem(label: 'Pressure', value: (psi1 != null )? "              $psi1 psi":"00000 psi " ?? "0", icon: Icons.speed),
                                               ),
                                               Padding(
                                                 padding: const EdgeInsets.only(left:20.0),
@@ -484,7 +484,7 @@ class _regenmode extends State<regenmode> {
                                               ),//°C
                                               Padding(
                                                 padding: const EdgeInsets.only(left:20.0),
-                                                child: InfoItem(label: 'Battery', value:(tpms_battery != null )? "                 $tpms_battery %":"00 % " ?? "0", icon: Icons.battery_full),
+                                                child: InfoItem(label: 'Battery', value:(tpms_battery != null )? "                $tpms_battery %":"00 % " ?? "0", icon: Icons.battery_full),
                                               ),
                                             ],
                                           ),
